c1c7e9d67f7c8c0a6ce8fed9f9e890636523bd28
Louis SUPPO <louis.suppo@artfx.fr>
2024-10-23 12:02:43 +0200

    (notfixeyet)

--- Assets/ZombieMouvement.cs
+++ Assets/ZombieMouvement.cs
@@ -5,26 +5,17 @@ using UnityEngine.TextCore.Text;
 
 public class ZombieMouvement : MonoBehaviour
 {
-
+    public float alpha;
     public Transform flics;
 
-    public float alpha;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         float opposite = (flics.transform.rotation.y - transform.rotation.y);
         float adjacent = (flics.transform.rotation.x - transform.rotation.x);
 
-
         alpha = Mathf.Atan2(opposite, adjacent) * (180 / Mathf.PI);
+
+
         transform.rotation  = Quaternion.Euler(0f , 0f, alpha);
     }
 }
